7830e857fe09390152c5833363fe25850613f4c2
JarvisDong <dongmm@handsure.com>
2018-08-16 09:06:52 +0800

    11

---
+++ jarvismall-manager/jarvismall-manager-dao/src/main/java/com/jarvismall/mapper/TbOrderMapper.java
@@ -0,0 +1,30 @@
+package com.jarvismall.mapper;
+
+import com.jarvismall.pojo.TbOrder;
+import com.jarvismall.pojo.TbOrderExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TbOrderMapper {
+    int countByExample(TbOrderExample example);
+
+    int deleteByExample(TbOrderExample example);
+
+    int deleteByPrimaryKey(String orderId);
+
+    int insert(TbOrder record);
+
+    int insertSelective(TbOrder record);
+
+    List<TbOrder> selectByExample(TbOrderExample example);
+
+    TbOrder selectByPrimaryKey(String orderId);
+
+    int updateByExampleSelective(@Param("record") TbOrder record, @Param("example") TbOrderExample example);
+
+    int updateByExample(@Param("record") TbOrder record, @Param("example") TbOrderExample example);
+
+    int updateByPrimaryKeySelective(TbOrder record);
+
+    int updateByPrimaryKey(TbOrder record);
+}
